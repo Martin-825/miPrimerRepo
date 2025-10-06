b3db2b6f89a8cd85b28f4873a94e4a4f3c0805cb
Martin-825 <juanmartincorrea@MacBook-Pro-de-Martin.local>
2025-10-05 21:49:32 -0300

    Mi primer commit

---
+++ code.c
@@ -0,0 +1,14 @@
+// INICIO ALGORITMO
+#include <stdio.h>
+
+void mensaje_bienvenida();
+
+int main(){
+    mensaje_bienvenida();
+    return(0);
+}
+
+void mensaje_bienvenida(){
+    printf("\nHola mundo!\n");
+}
+// FIN ALGORITMO
